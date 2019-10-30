c905958ce5afd861e5b50e998233f7a30f9194d7
ammarfqr <35327812+ammarfqr@users.noreply.github.com>
2019-10-30 10:30:37 +0800

    Add files via upload

---
+++ AMALICU03_K311FKPD002/connection.php
@@ -0,0 +1,8 @@
+<?php
+//DATABASE CONNECTION
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "stts";
+$conn=mysqli_connect($servername,$username ,$password ,$dbname );
+?>
